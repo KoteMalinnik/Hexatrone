47a7e159319661fe2905e96866c1702b7bd45ab6
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-23 03:28:29 +0300

    newClass(LoadingIndicatorRotation.cs) finish new class

---
+++ Assets/__Scripts/Animations/LoadingIndicatorRotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Loading indicator rotation.
+/// </summary>
+public class LoadingIndicatorRotation : MonoBehaviour
+{
+	Transform cachedTransform = null;
+
+	void Awake()
+	{
+		cachedTransform = GetComponent<Transform>();
+	}
+
+	/// <summary>
+	/// The delta angle of rotation Z axies.
+	/// </summary>
+	const float deltaAngle = 150;
+
+	Vector3 rotation = Vector3.zero;
+
+	void Update()
+	{
+		rotation = cachedTransform.localRotation.eulerAngles;
+		rotation.z += deltaAngle * Time.deltaTime;
+
+		cachedTransform.localRotation = Quaternion.Euler(rotation);
+	}
+}
