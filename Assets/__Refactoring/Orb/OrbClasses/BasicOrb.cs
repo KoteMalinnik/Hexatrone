540ddca46f4ca5c99f2aec9841c1671a61c2338b
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-24 04:19:15 +0300

    refactor(Orb) realise DeltaBonusOrb class (old enum CountBaf)
    move orb movement from BasicOrb to OrbMovement

--- Assets/__Refactoring/Orb/OrbClasses/BasicOrb.cs
+++ Assets/__Refactoring/Orb/OrbClasses/BasicOrb.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(OrbMovement))]
+[RequireComponent(typeof(SpriteRenderer))]
 /// <summary>
 /// Basic orb.
 /// </summary>
 public class BasicOrb : MonoBehaviour
 {
 	/// <summary>
-	/// Gets or sets the score delta.
+	/// Gets or sets the delta. Delta is a value which define deifference of the score on collecting orb
 	/// </summary>
-	/// <value>The score delta.</value>
-	public int scoreDelta { get; protected set; } = 0;
-
-	/// <summary>
-	/// Sets the score delta.
-	/// </summary>
-	/// <param name="scoreDelta">Score delta.</param>
-	public void setScoreDelta(int scoreDelta) { this.scoreDelta = scoreDelta; }
+	public int delta { get; protected set; } = 1;
 
 	/// <summary>
 	/// Gets the color of the orb.
 	/// </summary>
-	/// <value>The color of the orb.</value>
 	public Color orbColor { get; private set; } = Color.white;
+
+
+	/// <summary>
+	/// Sets the delta.
+	/// </summary>
+	/// <param name="delta"> delta.</param>
+	public void setDelta(int delta) { this.delta = delta; }
 
 	/// <summary>
 	/// Sets the color of the orb.
@@ -29,31 +30,17 @@ public class BasicOrb : MonoBehaviour
 	/// <param name="orbColor">Orb color.</param>
 	public void setOrbColor(Color orbColor) { this.orbColor = orbColor; }
 
-	Transform cachedTransform = null;
+	/// <summary>
+	/// Sets the sprite.
+	/// </summary>
+	/// <param name="sprite">Sprite.</param>
+	public void setSprite(Sprite sprite) { spriteRenerer.sprite = sprite; }
+
+
+	SpriteRenderer spriteRenerer = null;
 
 	void Awake()
 	{
-		cachedTransform = transform;
-	}
-
-	[SerializeField, Range(0.01f, 10f)]
-	/// <summary>
-	/// The movement speed of gameObject.
-	/// </summary>
-	float movementSpeed = 1f;
-
-	/// <summary>
-	/// The target position.
-	/// </summary>
-	readonly Vector3 targetPosition = new Vector3(0, -3, 0);
-
-	void Update()
-	{
-		if(!Statements.pause)
-		{
-			var currentPosition = cachedTransform.localPosition;
-			currentPosition = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
-			cachedTransform.localPosition = currentPosition;
-		}
+		spriteRenerer = GetComponent<SpriteRenderer>();
 	}
 }
---
+++ Assets/__Refactoring/Orb/OrbMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Orb movement.
+/// </summary>
+public class OrbMovement : MonoBehaviour
+{
+	Transform cachedTransform = null;
+
+	void Awake()
+	{
+		cachedTransform = transform;
+	}
+
+	[SerializeField, Range(0.01f, 10f)]
+	/// <summary>
+	/// The movement speed of gameObject.
+	/// </summary>
+	float movementSpeed = 2f;
+
+	/// <summary>
+	/// The target position.
+	/// </summary>
+	readonly Vector3 targetPosition = new Vector3(0, -3, 0);
+
+	void Update()
+	{
+		if (!Statements.pause)
+		{
+			var currentPosition = cachedTransform.localPosition;
+			currentPosition = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
+			cachedTransform.localPosition = currentPosition;
+		}
+	}
+}
