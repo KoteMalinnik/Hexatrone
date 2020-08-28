11d3f1c36681e5cebdef7caee15e8605cc3d09ab
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-29 01:18:05 +0300

    refactor preload scene scripts

---
+++ Assets/Scripts/Preload Scene Controll/PreloadSceneController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadSceneController : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] bool loadNextScene = true;
+    [SerializeField] string nextSceneName = "1_MainMenu";
+    #endregion
+
+    void Start()
+	{
+        if (loadNextScene)
+        {
+            Log.Message("Загрузка параметров.");
+            Serialization.loadAllParametrs();
+            Log.Message("Загрузка параметров завершена.");
+
+            Log.Message("Загрузка следующей сцены: " + nextSceneName);
+            SceneManager.LoadSceneAsync(nextSceneName);
+        }
+	}
+}
