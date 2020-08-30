eb25e29fb28fe9084fcc914a64f5cc2c01395c25
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-30 03:57:59 +0300

    refactor OrbCollision and rename it to Orb.CollisionListener

---
+++ Assets/Scripts/Game/Orb/OrbCollisionListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Orb
+{
+	public class CollisionListener : MonoBehaviour
+	{
+		public static event Action<Color> OnOrbCollisionWithPart = null;
+
+		void OnTriggerEnter2D(Collider2D collision)
+		{
+			Log.Message("Столкновение с " + collision.name);
+
+			Color collisedPartColor = collision.GetComponent<ColorController>().Color;
+			OnOrbCollisionWithPart?.Invoke(collisedPartColor);
+		}
+	}
+}
