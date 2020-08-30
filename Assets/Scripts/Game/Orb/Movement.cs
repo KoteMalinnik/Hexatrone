1dc0adf210e31130c7f73012df89348a1072072a
Nikita Kalinnikov <niki9888985@mail.ru>
2020-08-30 04:01:05 +0300

    refactor OrbMovement and rename it to Orb.Movement

---
+++ Assets/Scripts/Game/Orb/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Orb
+{
+	public class Movement : MonoBehaviour
+	{
+		#region Fields
+		[Range(0.01f, 10f)]
+		[SerializeField] float movementSpeed = 2f;
+
+		[SerializeField] Vector3 targetPosition = new Vector3(0, -9, 0);
+
+		Transform cachedTransform = null;
+		#endregion
+
+		void Awake()
+		{
+			cachedTransform = transform;
+		}
+
+		void Update()
+		{
+			if (!Statements.pause)
+			{
+				var currentPosition = cachedTransform.localPosition;
+				currentPosition = Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime);
+				cachedTransform.localPosition = currentPosition;
+			}
+		}
+	}
+}
