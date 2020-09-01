c6569efa256f5df481782a16452b4a0803179e40
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-02 00:28:27 +0300

    move same orb and form logic outside

---
+++ Assets/Scripts/Core/ObjectInstantiator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class ObjectInstantiator
+{
+	public static GameObject Instantiate
+		(
+		GameObject prefab,
+		Transform parent,
+		Vector2 position,
+		Quaternion rotation
+		)
+	{
+		Log.Message("Создание формы: " + prefab?.name);
+
+		if (prefab == null)
+		{
+			Log.Warning("Префаб пуст.");
+			return null;
+		}
+
+		GameObject form = MonoBehaviour.Instantiate(prefab, position, rotation, parent);
+		return form;
+	}
+}
