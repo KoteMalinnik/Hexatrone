65b80e5d3872f417a15748c4a63a6a226f255c2a
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-03 00:59:45 +0300

    refactor missed orbs at form level counter

---
+++ Assets/Scripts/Game/Orb Counters/MissedOrbsAtFormLevelCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Counters;
+
+namespace OrbCounters
+{
+	public class MissedOrbsAtFormLevelCounter
+	{
+		#region Properties
+		DescendingCounter counter { get; } = null;
+		public ushort Value => counter.Value;
+		#endregion
+
+		public MissedOrbsAtFormLevelCounter(ushort orbsAllowedToMissAtFormLevelCount, Action OnAllAllowedOrbsMissed)
+		{
+			counter = new DescendingCounter(orbsAllowedToMissAtFormLevelCount, "MissedOrbsAtFormLevel", 0);
+			counter.OnMinValueReach += OnAllAllowedOrbsMissed;
+		}
+
+		public void Subtract(ushort delta = 1)
+		{
+			counter.Subtract(delta);
+		}
+
+		public void AddListenerOnValueChanged(Action<int> action)
+		{
+			counter.OnValueChanged += action;
+		}
+
+		public void RemoveListenerOnValueChanged(Action<int> action)
+		{
+			counter.OnValueChanged -= action;
+		}
+	}
+}
