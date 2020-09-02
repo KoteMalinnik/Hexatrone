b09e363cb7caf9f3acc8102c2e1dbbdf97398fb1
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-03 00:42:15 +0300

    add counter OnValueChanged event

--- Assets/Scripts/Game/Orb Counters/Core/BaseCounter.cs
+++ Assets/Scripts/Game/Orb Counters/Core/BaseCounter.cs
@@ -4,9 +4,29 @@ namespace Counters
 {
     public abstract class BaseCounter
     {
+		#region Events
+		public event Action<int> OnValueChanged = null;
+		#endregion
+
+		#region Fields
+		ushort value = 0;
+		#endregion
+
 		#region Properties
 		protected string ID { get; }  = "Counter";
-		public ushort Value { get; protected set; } = 0;
+
+		public ushort Value
+		{
+			get
+            {
+				return value;
+            }
+			protected set
+            {
+				this.value = value;
+				OnValueChanged?.Invoke(value);
+            }
+		}
 		#endregion
 
 		public BaseCounter(ushort initialValue, string ID)
