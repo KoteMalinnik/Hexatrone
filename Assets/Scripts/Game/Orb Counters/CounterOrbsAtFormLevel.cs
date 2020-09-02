5359fbd71de06510f6b6d1bb087bf0fcaf117f60
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-03 00:25:37 +0300

    rename Counters folder to Orb Counters

--- Assets/Scripts/Game/Orb Counters/CounterOrbsAtFormLevel.cs
+++ Assets/Scripts/Game/Orb Counters/CounterOrbsAtFormLevel.cs
@@ -1,19 +1,9 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Счетчик сфер на конкретном уровне формы. Обновляется при новом уровне.
-/// </summary>
 public static class CounterOrbsAtFormLevel
 {
-	/// <summary>
-	/// Значение.
-	/// </summary>
 	public static int value { get; private set; } = 0;
 
-	/// <summary>
-	/// Установить значение.
-	/// </summary>
-	/// <param name="newValue">Новое значение.</param>
 	public static void setValue(int newValue)
 	{
 		if (newValue < 0) return;
@@ -31,31 +21,16 @@ public static class CounterOrbsAtFormLevel
 		Debug.Log("[CounterOrbsAtFormLevel] Сфер на уровне формы: " + value);
 	}
 
-	/// <summary>
-	/// Инкрементировать значение.
-	/// </summary>
-	/// <param name="delta">Сколько прибавить.</param>
 	public static void incrementValue(int delta = 1)
 	{
 		setValue(value + delta);
 		CounterTotalOrbs.incrementValue(delta);
 	}
 
-	/// <summary>
-	/// Декрементировать значение.
-	/// </summary>
-	/// <param name="delta">Сколько отнять.</param>
 	public static void decrementValue(int delta = 1) { setValue(value - delta); }
 
-	/// <summary>
-	/// Количество сфер до следующего повышения уровня.
-	/// </summary>
 	public static int valueToLevelUp { get; private set; } = 10;
 
-	/// <summary>
-	/// Установить количество сфер до следующего повышения уровня.
-	/// </summary>
-	/// <param name="formLevel">Form level.</param>
 	public static void setValueToLevelUp(int formLevel)
 	{
 		valueToLevelUp = 10 * (formLevel * 2 + 1); // рассчет количества сфер до следующего уровня
---
+++ Assets/Scripts/Game/Orb Counters/CounterTotalOrbs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace OrbCounters
+{
+	public static class CounterTotalOrbs
+	{
+		public static int value { get; private set; } = 0;
+
+		public static void setValue(int newValue)
+		{
+			if (newValue < 0) return;
+
+			value = newValue;
+			Debug.Log("[CounterTotalOrbs] Общее количество сфер: " + value);
+		}
+
+		public static void incrementValue(int delta = 1) { setValue(value + delta); }
+
+		public static void decrementValue(int delta = 1) { setValue(value - delta); }
+	}
+}
