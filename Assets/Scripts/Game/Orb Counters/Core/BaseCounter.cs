586084bfd0857cc80fc7e36a82d85c014b3cf462
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-03 00:25:18 +0300

    add counters logic

---
+++ Assets/Scripts/Game/Orb Counters/Core/BaseCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Counters
+{
+    public abstract class BaseCounter
+    {
+		#region Properties
+		protected string ID { get; }  = "Counter";
+		public ushort Value { get; protected set; } = 0;
+		#endregion
+
+		public BaseCounter(ushort initialValue, string ID)
+		{
+			if (string.IsNullOrEmpty(ID)) throw new ArgumentNullException();
+			if (initialValue < 0) throw new ArgumentOutOfRangeException();
+
+			Value = initialValue;
+			this.ID = ID;
+
+			Log.Message($"Инициализация счетчика {ID} с начальным значением: {Value}.");
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Orb Counters/Core/DescendingCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Counters
+{
+    /// <summary>
+    /// Нисходящий счетчик.
+    /// Считает от установленного начального значения только в отрицательную сторону.
+    /// При достижении минимального значения будет вызвано событие.
+    /// По умолчанию минимального значение составляет 0.
+    /// </summary>
+    public class DescendingCounter : BaseCounter
+    {
+        #region Events
+        public event Action OnMinValueReach = null;
+        #endregion
+
+        #region Fields
+        ushort minValue = 0;
+        #endregion
+
+        public DescendingCounter(ushort initialValue, string ID, ushort minValue = 0) : base(initialValue, ID)
+        {
+            this.minValue = minValue;
+            Log.Message($"Установка минимального значения счетчика {ID}: " + minValue);
+        }
+
+        public void Subtract(ushort delta = 1)
+        {
+            if (Value <= minValue)
+            {
+                Log.Message($"Счетчик {ID} достиг минимального значения. Невозможно вычесть.");
+                return;
+            }
+
+            Value -= delta;
+            Log.Message($"Вычитание из счетчика {ID}: {Value + delta} - {delta} -> {Value}.");
+
+            if (Value <= minValue)
+            {
+                Log.Message($"Счетчик {ID} достиг минимального значения.");
+                OnMinValueReach?.Invoke();
+            }
+        }
+    }
+}
