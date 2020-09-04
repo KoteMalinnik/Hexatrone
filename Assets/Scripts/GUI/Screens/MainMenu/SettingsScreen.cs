5d1e321356b0686189019b74895521b7da3b6901
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-05 02:54:26 +0300

    refactor settings screen

--- Assets/Scripts/GUI/Screens/MainMenu/SettingsScreen.cs
+++ Assets/Scripts/GUI/Screens/MainMenu/SettingsScreen.cs
@@ -1,80 +1,87 @@
 ﻿using UnityEngine;
 using CustomScreen.Core;
 using UnityEngine.UI;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using System;
 
 namespace CustomScreen
 {
     public class SettingsScreen : BaseScreen
     {
-		[Header("GUI Audiobutton's images")]
+		#region Events
+		public static event Action<bool> OnMusicToggleChanged = null;
+		public static event Action<bool> OnSoundToggleChanged = null;
+        #endregion
 
-		[SerializeField]
-		/// <summary>
-		/// Image кнопки музыки.
-		/// </summary>
-		Image imageMusicButton = null;
+        #region Fields
+        [Header("Кнопки")]
+		[SerializeField] Button button_Music = null;
+		[SerializeField] Button button_Sound = null;
 
-		[SerializeField]
-		/// <summary>
-		/// Image кнопки звука.
-		/// </summary>
-		Image imageSoundButton = null;
+		[Header("Цвета вкл/выкл")]
+		[SerializeField] Color c_allowAudio = Color.blue;
+		[SerializeField] Color c_disallowAudio = Color.red;
 
-		[Header("GUI Audiobutton's colors")]
+		bool state_Sound = true;
+		bool state_Music = true;
+        #endregion
 
-		[SerializeField]
-		/// <summary>
-		/// Цвет при разрешенном воспроизведении аудио.
-		/// </summary>
-		Color allowAudioColor = Color.blue;
+        #region MonoBehaviour Callbacks
+        private void OnEnable()
+        {
+			Log.Message("Загрузка и выставление значений.");
+			button_Music.interactable = true;
+			button_Sound.interactable = true;
 
-		[SerializeField]
-		/// <summary>
-		/// Цвет при запрещенном воспроизведении аудио.
-		/// </summary>
-		Color disallowAudioColor = Color.red;
+			state_Music = Serialization.Load(SerializationKeys.MusicState, true);
+			state_Sound = Serialization.Load(SerializationKeys.SoundState, true);
 
-		// Установить начальные цвета кнопок.
-		void Start()
-		{
-			//if (MusicManager.instance != null) imageMusicButton.color = MusicManager.instance.allowAudio ? allowAudioColor : disallowAudioColor;
-			//if (SoundManager.instance != null) imageSoundButton.color = SoundManager.instance.allowAudio ? allowAudioColor : disallowAudioColor;
+			SetupButtonColor(button_Music, state_Music);
+			SetupButtonColor(button_Sound, state_Sound);
 		}
+        #endregion
+
+        #region Screens Open Methods
+        public void OpenMainMenuScreen()
+		{
+			Log.Message("Нажатие на кнопку <Back>.");
+			ScreenManager.OpenScreen(ScreenType.MainMenuScreen);
+		}
+		#endregion
 
 		/// <summary>
 		/// Переключить звук.
 		/// </summary>
-		public void __Sound()
+		public void ToggleSound()
 		{
-			//if (SoundManager.instance == null) return;
+			Log.Message("Переключение звука.");
 
-			Debug.Log("[MainMenuGUI] Переключение звука.");
+			state_Sound = ToggleAudio(SerializationKeys.SoundState, button_Sound, state_Sound);
+			OnSoundToggleChanged?.Invoke(state_Sound);
+        }
 
-			var state = imageSoundButton.color == allowAudioColor;
-			state = !state;
-			//SoundManager.instance.setAudioState(state);
+        /// <summary>
+        /// Переключить музыку.
+        /// </summary>
+        public void ToggleMusic()
+		{
+			Log.Message("Переключение музыки.");
 
-			imageSoundButton.color = state ? allowAudioColor : disallowAudioColor;
+			state_Music = ToggleAudio(SerializationKeys.MusicState, button_Music, state_Music);
+			OnMusicToggleChanged?.Invoke(state_Music);
 		}
 
-		/// <summary>
-		/// Переключить музыку.
-		/// </summary>
-		public void __Music()
-		{
-			//if (MusicManager.instance == null) return;
-
-			Debug.Log("[MainMenuGUI] Переключение музыки.");
-
-			var state = imageMusicButton.color == allowAudioColor;
+		bool ToggleAudio(SerializationKeys key, Button button, bool state)
+        {
 			state = !state;
-			//MusicManager.instance.setAudioState(state);
+			SetupButtonColor(button, state);
+			Serialization.Save(key, state);
 
-			imageMusicButton.color = state ? allowAudioColor : disallowAudioColor;
+			return state;
+		}
+
+		void SetupButtonColor(Button button, bool state)
+		{
+			button.GetComponent<Image>().color = state ? c_allowAudio : c_disallowAudio;
 		}
 	}
 }
