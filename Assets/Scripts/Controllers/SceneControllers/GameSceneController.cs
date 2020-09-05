afd020853f60fdc5f1110ba6deee64f68dd8e1f6
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-05 03:49:53 +0300

    add orbCounters prefab to load by GameSceneController.cs

--- Assets/Scripts/Controllers/SceneControllers/GameSceneController.cs
+++ Assets/Scripts/Controllers/SceneControllers/GameSceneController.cs
@@ -7,13 +7,17 @@ using UnityEngine;
 public class GameSceneController : MonoBehaviour
 {
     #region Fields
+	[Range(1, 6)]
     [SerializeField] int startingLevel = 0;
 	[SerializeField] bool createFormOnStart = true;
-
 	[Space]
-
 	[SerializeField] bool pauseOnStart = true;
+	[Space]
+	[SerializeField] GameObject orbCountersPrefab = null;
+	[SerializeField] Transform orbCountersParent = null;
+	[SerializeField] bool createOrbCounters = true;
 	#endregion
+
 	void Awake()
 	{
 		Log.Message("Настройка сцены..");
@@ -26,6 +30,11 @@ public class GameSceneController : MonoBehaviour
 				formLevelController.SetupLevel(startingLevel);
 			}
 		}
+
+		if (createOrbCounters)
+        {
+			Instantiate(orbCountersPrefab, orbCountersParent).name = "OrbCounters";
+        }
 
 		Statements.Pause = pauseOnStart;
 		Statements.GameOver = false;
