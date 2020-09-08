58b0bb8b4a4548acbc5d90020ca8c9b2c12aa1ad
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-09 01:20:45 +0300

    rename PauseGUI -> PauseScreen

---
+++ Assets/Scripts/GUI/Screens/Game/PauseScreen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using CustomScreen.Core;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace CustomScreen.Game
+{
+    public class PauseScreen : BaseScreen
+    {
+		#region Fields
+		[SerializeField] SceneAsset mainMenuScene = null;
+		#endregion
+
+		public void RestartLevel()
+        {
+			Log.Message("Нажатие на кнопку <RestartLevel>.");
+			SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+		}
+
+		public void LoadMainMenu()
+        {
+			Log.Message("Нажатие на кнопку <ToMainMenu>.");
+			SceneManager.LoadSceneAsync(mainMenuScene.name);
+		}
+
+		#region Screens Open Methods
+		public void OpenSettingsScreen()
+		{
+			Log.Message("Нажатие на кнопку <Settings>.");
+			ScreenManager.OpenScreen(ScreenType.SettingsScreen);
+			//TODO: SettingsScreen при нажатии на кнопку "НАЗАД" будет показывать MainScreen
+		}
+		#endregion
+	}
+}
