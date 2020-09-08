c6b70336587f15550d7b369d22cff03905344e57
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-09 01:20:34 +0300

    rename GameOverGUI -> GameOverScreen

---
+++ Assets/Scripts/GUI/Screens/Game/GameOverScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using CustomScreen.Core;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameOverScreen : BaseScreen
+{
+	//TODO: описать экран проигрыша.
+}
