c671d91543d4e278094def13dedef8006d0c2ccc
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 01:35:27 +0300

    add error catching on collision

--- Assets/Scripts/Controllers/OrbController/OrbBehaviours/OrbCollisionListener.cs
+++ Assets/Scripts/Controllers/OrbController/OrbBehaviours/OrbCollisionListener.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using OrbCollision;
+using Part;
 
 namespace Orb
 {
@@ -15,11 +16,45 @@ namespace Orb
 		{
 			Log.Message("Столкновение с " + collision.name);
 
-			Color partColor = collision.GetComponent<SpriteRenderer>().color;
-			Color orbColor = GetComponent<SpriteRenderer>().color;
-			Part.PartFlashing partFlashing = collision.GetComponent<Part.PartFlashing>();
+			Color partColor = Color.black;
+			Color orbColor = Color.black;
+			PartFlashing partFlashing = null;
 
-			OnCollision?.Invoke(new CollisionData(partColor, orbColor, partFlashing));
+			SpriteRenderer spriteRendererChecker = null;
+			
+			if (collision.TryGetComponent(out spriteRendererChecker))
+            {
+				partColor = spriteRendererChecker.color;
+            }
+			else
+            {
+				Log.Error("Не удалось получить компонент SpriteRenderer части формы: " + collision.name);
+            }
+
+			if (!collision.TryGetComponent(out partFlashing))
+			{
+				Log.Error("Не удалось получить компонент PartFlashing части формы: " + collision.name);
+			}
+
+			if (TryGetComponent(out spriteRendererChecker))
+			{
+				orbColor = spriteRendererChecker.color;
+			}
+			else
+			{
+				Log.Error("Не удалось получить компонент SpriteRenderer сферы.");
+			}
+
+			try
+            {
+				CollisionData collisionData = new CollisionData(partColor, orbColor, partFlashing);
+				OnCollision?.Invoke(collisionData);
+			}
+            catch
+            {
+				Log.Warning("Обработка ошибки генерации данных о колизии.");
+				Debug.Break();
+            }
 
 			Log.Message("Уничтожение сферы.");
 			Destroy(gameObject);
