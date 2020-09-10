f71ac8c03e8d8c397109ad5c21d4ca8329a74d3f
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 00:18:12 +0300

    add GameOver screen logic

--- Assets/Scripts/UI/Screens/Game/GameOverScreen.cs
+++ Assets/Scripts/UI/Screens/Game/GameOverScreen.cs
@@ -8,5 +8,19 @@ using UnityEditor;
 
 public class GameOverScreen : BaseScreen
 {
-	//TODO: описать экран проигрыша.
+	#region Fields
+	[SerializeField] SceneAsset mainMenuScene = null;
+	#endregion
+
+	public void Restart()
+	{
+		Log.Message("Нажатие на кнопку <Restart>.");
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+	}
+
+	public void LoadMainMenu()
+	{
+		Log.Message("Нажатие на кнопку <ToMainMenu>.");
+		SceneManager.LoadSceneAsync(mainMenuScene.name);
+	}
 }
