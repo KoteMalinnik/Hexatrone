96115005b474ff35993c8c3bc10c7b46e31e061d
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 01:15:09 +0300

    add argument null exception to CollisionData constructor

--- Assets/Scripts/Controllers/OrbController/OrbCollisionHandler/CollisionData.cs
+++ Assets/Scripts/Controllers/OrbController/OrbCollisionHandler/CollisionData.cs
@@ -13,6 +13,10 @@ namespace OrbCollision
 
         public CollisionData(Color PartColor, Color OrbColor, Part.PartFlashing PartFlashing)
         {
+            if (PartColor == null) throw new System.ArgumentNullException();
+            if (OrbColor == null) throw new System.ArgumentNullException();
+            if (PartFlashing == null) throw new System.ArgumentNullException();
+
             this.PartColor = PartColor;
             this.OrbColor = OrbColor;
 
