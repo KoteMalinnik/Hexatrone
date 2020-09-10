e6fd059c7ec6b1014935bf5cf3bf90952984a090
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 00:40:36 +0300

    add OnPrepareTapButtonDown event

--- Assets/Scripts/UI/Screens/Game/PrepareTapScreen.cs
+++ Assets/Scripts/UI/Screens/Game/PrepareTapScreen.cs
@@ -1,15 +1,21 @@
 ﻿using CustomScreen.Core;
+using System;
 
 namespace CustomScreen.Game
 {
     public class PrepareTapScreen : BaseScreen
     {
+		#region Events
+		public static event Action OnPrepareTapButtonDown = null;
+        #endregion
+
         #region Screens Open Methods
         public void CloseThisScreen()
 		{
 			Log.Message("Нажатие на кнопку <PrepareTap>.");
 			ScreenManager.CloseScreen(ScreenType);
 			Statements.Pause = false;
+			OnPrepareTapButtonDown?.Invoke();
 		}
 		#endregion
 	}
