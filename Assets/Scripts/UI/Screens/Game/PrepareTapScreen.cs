c799eeef18adcf0cd537bed96d75276172a0a846
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 00:20:20 +0300

    add PrepareTapScreen (old Prestart button)

---
+++ Assets/Scripts/UI/Screens/Game/PrepareTapScreen.cs
@@ -0,0 +1,17 @@
+﻿using CustomScreen.Core;
+using System;
+
+namespace CustomScreen.Game
+{
+    public class PrepareTapScreen : BaseScreen
+    {
+        #region Screens Open Methods
+        public void CloseThisScreen()
+		{
+			Log.Message("Нажатие на кнопку <PrepareTap>.");
+			ScreenManager.CloseScreen(ScreenType);
+			Statements.Pause = false;
+		}
+		#endregion
+	}
+}
