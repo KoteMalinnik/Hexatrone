345b538e0e82723eeb8ddd5b3966423dcd8240dd
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 00:42:02 +0300

    add OnPrepareTapButtonDown event handler (initializing form)

--- Assets/Scripts/Controllers/SceneControllers/GameSceneController.cs
+++ Assets/Scripts/Controllers/SceneControllers/GameSceneController.cs
@@ -1,4 +1,5 @@
-﻿using Form;
+﻿using CustomScreen.Game;
+using Form;
 using UnityEngine;
 
 /// <summary>
@@ -9,16 +10,19 @@ public class GameSceneController : MonoBehaviour
     #region Fields
 	[Range(1, 6)]
     [SerializeField] int startingLevel = 0;
+#if UNITY_EDITOR
 	[SerializeField] bool createFormOnStart = true;
+#endif
 	[Space]
 	[SerializeField] bool pauseOnStart = true;
 	[Space]
 	[SerializeField] GameObject orbCountersPrefab = null;
 	[SerializeField] Transform orbCountersParent = null;
 	[SerializeField] bool createOrbCounters = true;
-	#endregion
+    #endregion
 
-	void Awake()
+    #region MonoBehaviour Callbacks
+    void Awake()
 	{
 		Log.Message("Настройка сцены..");
 
@@ -30,16 +34,41 @@ public class GameSceneController : MonoBehaviour
 		Statements.Pause = pauseOnStart;
 		Statements.GameOver = false;
 
+#if UNITY_EDITOR
 		if (createFormOnStart)
 		{
-			var formLevelController = FindObjectOfType<FormLevelController>();
-			if (formLevelController != null)
-			{
-				formLevelController.SetupLevel(startingLevel);
-			}
+			InitializeForm();
 		}
+#endif
+	}
 
+    private void OnEnable()
+    {
+		PrepareTapScreen.OnPrepareTapButtonDown += InitializeForm;
+    }
+
+    private void OnDisable()
+    {
+		PrepareTapScreen.OnPrepareTapButtonDown -= InitializeForm;
+	}
+
+    private void OnDestroy()
+    {
 		Log.Message("Настройка сцены завершена.");
+	}
+    #endregion
+
+    private void InitializeForm()
+    {
+		var formLevelController = FindObjectOfType<FormLevelController>();
+		if (formLevelController != null)
+		{
+			formLevelController.SetupLevel(startingLevel);
+		}
+        else
+        {
+			Log.Warning("Попытка инициализации формы, но FormLevelController отсутствует на сцене.");
+        }
 
 		Destroy(gameObject);
 	}
