99a7ecd919d618dc70b4fd50a3d57ceb39f04478
Nikita Kalinnikov <niki9888985@mail.ru>
2020-09-11 15:40:29 +0300

    get outside base logic of TextViewers to IntTextViewer.cs

---
+++ Assets/Scripts/UI/Screens/Game/GameInfo/Logic/TextViewers/Base/IntTextViewer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class IntTextViewer : MonoBehaviour
+{
+	#region Fields
+	[SerializeField] Text text_value = null;
+	#endregion
+
+	#region Properties
+	protected Text TextValue => text_value;
+	#endregion
+
+	#region Abstract Methods
+	protected abstract void UpdateText(int newValue);
+
+	protected abstract void OnEnable();
+	protected abstract void OnDisable();
+	#endregion
+}
---
+++ Assets/Scripts/UI/Screens/Game/GameInfo/Logic/TextViewers/TotalOrbsCollectedCountTextViewer.cs
@@ -0,0 +1,25 @@
+﻿using OrbCounters;
+
+namespace CustomScreen.Logic
+{
+    public class TotalOrbsCollectedCountTextViewer : IntTextViewer
+    {
+        #region Overrided Methods
+        protected override void OnEnable()
+        {
+            CollectedOrbsInTotalCounter.OnNewOrbCollected += UpdateText;
+        }
+
+        protected override void OnDisable()
+        {
+            CollectedOrbsInTotalCounter.OnNewOrbCollected -= UpdateText;
+        }
+
+        protected override void UpdateText(int orbsCollecrtedInTotalCount)
+        {
+            Log.Message("Обновление текста " + typeof(TotalOrbsCollectedCountTextViewer));
+            TextValue.text = orbsCollecrtedInTotalCount.ToString();
+        }
+        #endregion
+    }
+}
