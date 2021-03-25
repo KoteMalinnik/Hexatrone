608e4994cd8adf6ff3aaad0aa04791a2766e7f9d
Nikita Kalinnikov <niki9888985@mail.ru>
2021-03-26 01:10:36 +0300

    implement IEnumerable in ColorSet color list

--- Assets/Scripts/Core/ScriptableObjects/ColorSet.cs
+++ Assets/Scripts/Core/ScriptableObjects/ColorSet.cs
@@ -1,15 +1,34 @@
-﻿using UnityEngine;
+﻿/*
+ * Класс создает сериализуемый объект, который содержит возможные цвета сфер
+ */
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 [CreateAssetMenu(fileName = "NewColorSet", menuName = "ColorSet", order = 51)]
-public class ColorSet: ScriptableObject
+public class ColorSet: ScriptableObject, IEnumerable<Color>
 {
-    [SerializeField] Color[] colors = new Color[0];
+    [SerializeField] private List<Color> colors = new List<Color>();
 
-    public int Lenght => colors.Length;
+    public int Count => colors.Count;
 
     public Color GetColor(int index)
     {
-        if (index < 0 || index > Lenght - 1) throw new System.ArgumentOutOfRangeException();
+        if (index < 0 || index >= Count) throw new System.ArgumentOutOfRangeException();
+        
         return colors[index];
+    }
+
+    // IEnumerable<Color> interface implementation
+
+    public IEnumerator<Color> GetEnumerator()
+    {
+        return ((IEnumerable<Color>)colors).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)colors).GetEnumerator();
     }
 }
