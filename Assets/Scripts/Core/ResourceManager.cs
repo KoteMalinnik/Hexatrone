c3b16542b3ddc853748024ef47feacd5972758dc
Nikita Kalinnikov <niki9888985@mail.ru>
2021-07-06 00:59:52 +0300

    refactor ResourceLoader -> ResourceManager

---
+++ Assets/Scripts/Core/ResourceManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace Core
+{
+	interface IResourceManager
+	{
+		bool TryLoad<T>(string path, string name, out T result) where T : Object;
+	}
+
+	public class ResourceManager : IResourceManager
+	{
+        public bool TryLoad<T>(string path, string name, out T result) where T : Object
+        {
+			Log.Message($"Загрузка объекта \"{name}\" ({typeof(T).Name}) из ресурсов по пути: {path}.");
+
+			var fullPath = Path.Combine(path, name);
+			result = Resources.Load<T>(fullPath);
+
+			if (result is null)
+            {
+				Log.Warning($"Ресурс по пути {path} отсутствует!");
+				return false;
+			}
+
+			return true;
+		}
+    }
+
+}
