e00dfc10d96232176c51496348d185bc833f2ea8
Nikita Kalinnikov <niki9888985@mail.ru>
2021-07-19 21:49:49 +0300

    add PauseManager interface

---
+++ Assets/Scripts/IPauseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StatementManagers
+{
+    interface IPauseManager
+    {
+        event Action OnPause;
+        event Action OnUnpause;
+
+        bool IsPause { get; }
+
+        void Pause();
+        void Unpause();
+    }
+}
