9faa7721a7442a8f078189cb090c1d29a895125d
Nikita Kalinnikov <niki9888985@mail.ru>
2021-07-19 22:17:26 +0300

    add Counter interface

---
+++ Assets/Scripts/Core/Counter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core
+{
+    interface ICounter
+    {
+        event Action OnEndValueReached;
+
+        int StartValue { get; }
+        int EndValue { get; }
+
+        void ChangeValue();
+        void Reset();
+    }
+}
